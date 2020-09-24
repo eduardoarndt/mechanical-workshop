f9ddd25b3a6078e3c0456163b7a514679871146d
Eduardo Arndt <eduardo-arndt@hotmail.com>
2020-09-24 20:39:50 -0300

    fix typos

--- src/models/Person.py
+++ src/models/Person.py
@@ -1,20 +1,20 @@
 class _Person:
-    def __init__(self, name, cpf, numer, mail):
+    def __init__(self, name, cpf, number, mail):
         self.name = name
         self.cpf = cpf
-        self.numer = numer
+        self.number = number
         self.mail = mail
 
 
 class Employee(_Person):
-    def __init__(self, name, cpf, numer, mail):
-        super().__init__(name, cpf, numer, mail)
+    def __init__(self, name, cpf, number, mail):
+        super().__init__(name, cpf, number, mail)
 
 
 class Customer(_Person):
-    def __init__(self, name, cpf, numer, mail, addres, birthDate, gender, civilState, isCustomerActive):
-        super().__init__(name, cpf, numer, mail)
-        self.addres = addres
+    def __init__(self, name, cpf, number, mail, address, birthDate, gender, civilState, isCustomerActive):
+        super().__init__(name, cpf, number, mail)
+        self.address = address
         self.birthDate = birthDate
         self.gender = gender
         self.civilState = civilState
