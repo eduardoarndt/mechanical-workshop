dae7ef196565f629ac30213381c687dd53f2806d
Eduardo Arndt <eduardo-arndt@hotmail.com>
2020-10-03 11:03:16 -0300

    Lucas' initial development of repository

---
+++ src/repository/DataBase.py
@@ -0,0 +1,27 @@
+from src.models.Person import *
+
+
+def singleton(cls, *args, **kw):
+    instances = {}
+
+    def _singleton():
+        if cls not in instances:
+            instances[cls] = cls(*args, **kw)
+        return instances[cls]
+
+    return _singleton
+
+
+@singleton
+class DataBase(object):
+    a = 1
+    employeeList = [
+        Employee("Lucas", "12345678951", 44449874, "lucas.cschuler@gmail.com"),
+        Employee("Lucas", "12345678951", 44449874, "lucas.cschuler@gmail.com")
+    ]
+
+    def __init__(self, x=0):
+        self.x = x
+
+    def appointmentsByDay(self, day):
+        return
