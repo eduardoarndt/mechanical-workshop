82e6b688b4c396ae500869365ad28ee1dadac6bb
Eduardo Arndt <eduardo-arndt@hotmail.com>
2020-10-08 22:33:52 -0300

    database enhancements

--- DataBase.py
+++ DataBase.py
@@ -1,9 +1,14 @@
 from Printer import *
+from ScheduleList import *
+
+scheduleList = SchedulesList()
 
 
 class DataBase(object):
     customers = []
     employees = []
+    appointments = []
+    idCounter = 0
 
     def listCustomers(self, printOnlyActives):
         printer = Printer()
@@ -16,6 +21,7 @@ class DataBase(object):
         printer.addPrintingObject("Birth date", True, 10)
         printer.addPrintingObject("Gender", False, 10)
         printer.addPrintingObject("Civil State", False, 15)
+        printer.printHeader()
 
         for customer in self.customers:
             if customer.isCustomerActive == printOnlyActives:
@@ -42,3 +48,25 @@ class DataBase(object):
                   printer.formatValue(1, employee.cpf) +
                   printer.formatValue(2, employee.number) +
                   printer.formatValue(3, employee.mail))
+
+    def freeSchedules(self):
+        printer = Printer()
+        printer.addPrintingObject("id", True, 10)
+        printer.addPrintingObject("Name", True, 25)
+        printer.addPrintingObject("Hour", True, 25)
+        printer.printHeader()
+
+        for employee in self.employees:
+            for hour in scheduleList.SCHEDULES:
+
+                foundFreeHour = False
+                for appointment in self.appointments:
+                    if appointment.employee == employee \
+                            and appointment.dateHour == hour \
+                            and appointment.dateHour + 1 == hour:
+                        foundFreeHour = True
+
+                if not foundFreeHour:
+                    print(printer.formatValue(0, employee.id) +
+                          printer.formatValue(0, employee.name) +
+                          printer.formatValue(1, hour))
