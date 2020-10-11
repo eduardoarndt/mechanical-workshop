7296721a5690e578d4b9af470909d9296c7c0732
Eduardo Arndt <eduardo-arndt@hotmail.com>
2020-10-11 18:45:34 -0300

    i don't even know what i'm doing anymore

--- DataBase.py
+++ DataBase.py
@@ -10,6 +10,20 @@ class DataBase(object):
     appointments = []
     idCounter = 0
 
+    def findEmployee(self, employeeId):
+        for employee in self.employees:
+            if employee.id == employeeId:
+                return employee
+            else:
+                print("Employee not found!")
+
+    def findCustomer(self, customerCpf):
+        for customer in self.customers:
+            if customer.cpf == customerCpf:
+                return customer
+            else:
+                print("Customer not found!")
+
     def listCustomers(self, printOnlyActives):
         printer = Printer()
 
@@ -49,24 +63,60 @@ class DataBase(object):
                   printer.formatValue(2, employee.number) +
                   printer.formatValue(3, employee.mail))
 
-    def freeSchedules(self):
+    def freeSchedules(self, dateOfService):
         printer = Printer()
-        printer.addPrintingObject("id", True, 10)
-        printer.addPrintingObject("Name", True, 25)
-        printer.addPrintingObject("Hour", True, 25)
+        printer.addPrintingObject("Option", True, 10)
+        printer.addPrintingObject("Id", True, 10)
+        printer.addPrintingObject("Name", True, 20)
+        printer.addPrintingObject("Date", False, 15)
+        printer.addPrintingObject("Hour", False, 10)
         printer.printHeader()
 
+        appointmentListForThatDay = []
+        for appointment in self.appointments:
+            if appointment.date == dateOfService:
+                appointmentListForThatDay.append(appointment)
+
+        freeScheduleOption = 0
+        freeSchedulesList = []
         for employee in self.employees:
             for hour in scheduleList.SCHEDULES:
 
-                foundFreeHour = False
-                for appointment in self.appointments:
+                foundFreeHour = True
+                for appointment in appointmentListForThatDay:
                     if appointment.employee == employee \
                             and appointment.dateHour == hour \
                             and appointment.dateHour + 1 == hour:
-                        foundFreeHour = True
+                        foundFreeHour = False
 
-                if not foundFreeHour:
-                    print(printer.formatValue(0, employee.id) +
-                          printer.formatValue(0, employee.name) +
-                          printer.formatValue(1, hour))
+                if foundFreeHour:
+                    freeScheduleOption += 1
+                    freeSchedulesList.append(DataToMarkAppoint(freeScheduleOption, employee.id, employee.name,
+                                                               dateOfService, hour))
+        for x in freeSchedulesList:
+            print(printer.formatValue(0, x.freeScheduleOption) +
+                  printer.formatValue(1, x.employeeId) +
+                  printer.formatValue(2, x.employeeName) +
+                  printer.formatValue(3, x.dateOfService) +
+                  printer.formatValue(4, x.hour))
+
+        return freeSchedulesList
+
+class DataToMarkAppoint:
+    freeScheduleOption = None
+    employeeId = None
+    employeeName = None
+    dateOfService = None
+    hour = None
+
+    def __init__(self, freeScheduleOption, employeeId, employeeName, dateOfService, hour):
+        self.freeScheduleOption = freeScheduleOption
+        self.employeeId = employeeId
+        self.employeeName = employeeName
+        self.dateOfService = dateOfService
+        self.hour = hour
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from DataBase import DataBase
-from models.Person import *
-from ScheduleList import *
+from models.Person import Customer, Employee
+from ScheduleList import SchedulesList
+from models.Appointment import Appointment, Status
+from models.Task import Task, AvailableTask
 
 repository = DataBase()
 scheduleList = SchedulesList()
@@ -60,10 +62,8 @@ def menuAppointments():
                        "2.List\n"))
 
     if (option == 1):
-        print("haujhsuahsuahsuahusahushausa")
-        # entrar com a data
-
-        retrieveEmployeeFreeSchedule()
+        dateOfService = input("Which date to register appointment? Please, type in format dd/mm/yy\n")
+        retrieveEmployeeFreeSchedule(dateOfService)
         # percorrer os serviços que tem data = a que foi informada
         # vou anotar qual funcionário é e qual horário é
 
@@ -72,14 +72,40 @@ def menuAppointments():
         # e segue
 
 
-def retrieveEmployeeFreeSchedule():
+# mds esse código tá um horror
+def retrieveEmployeeFreeSchedule(dateOfService):
+    # perguntar qual cliente
+    # salvar cpf to cliente
+
+    # perguntar qual task
+    availableTasks = AvailableTask()
+    #print this, ask which
+    availableTasks.getAllAvailableTasks()
+
+    taskChosen = availableTasks.getAvailableTask(1)
+
+
     print("Free schedules per employee")
-    repository.freeSchedules()
+    freeScheduleList = repository.freeSchedules(dateOfService)
+
+    chosenSchedule = int(input("Select the option that best fits"))
+    for options in freeScheduleList:
+        if options.freeScheduleOption == chosenSchedule:
+            repository.appointments.append(Appointment(options.dateOfService,
+                                                       options.hour,
+                                                       repository.findEmployee(options.employeeId),
+                                                       repository.findCustomer("CPF DO CUSTOMER"),
+                                                       Status.SCHEDULED,
+                                                       taskChosen
+                                                       ))
+
+    # should have one item if this worked!
+    print(repository.appointments)
 
 
 keep = True
 while keep:
-    option = int(input("'\nSelect the menu\n"
+    option = int(input("\nSelect the menu\n"
                        "1.Customer Management\n"
                        "2.Employee Management\n"
                        "3.Appointments Management\n"
